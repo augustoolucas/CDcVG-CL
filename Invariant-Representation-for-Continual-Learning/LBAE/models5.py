4dc02862c0f2f5780869b0cabd229d56336c00d8
lucas.a.alcantara <lucas.a.alcantara@accenture.com>
2021-06-30 11:11:48 -0300

    bigger discriminator

--- Invariant-Representation-for-Continual-Learning/LBAE/models5.py
+++ Invariant-Representation-for-Continual-Learning/LBAE/models5.py
@@ -77,16 +77,14 @@ class BinDropout(nn.Module):
         return x
 
 class Classifier(nn.Module):
-    def __init__(self):
+    def __init__(self, hps):
         super().__init__()
-        self.conv = nn.Sequential(nn.Conv2d(3, 16, kernel_size=3, stride=2, padding=1),
+        self.conv = nn.Sequential(nn.Conv2d(hps.channels, 16, kernel_size=3, stride=2, padding=1),
                                   nn.ELU(inplace=True),
-                                  nn.Conv2d(16, 32, kernel_size=3, stride=2, padding=1),
+                                  nn.Conv2d(16, 32, kernel_size=3, stride=3, padding=0),
                                   nn.ELU(inplace=True))
 
-        self.linear = nn.Sequential(nn.Linear(2048, 512),
-                                    nn.ELU(inplace=True),
-                                    nn.Linear(512, 10),
+        self.linear = nn.Sequential(nn.Linear(800, 10),
                                     nn.Softmax(dim=1))
 
     def forward(self, x):
@@ -96,16 +94,18 @@ class Classifier(nn.Module):
         return x
 
 class Discriminator(nn.Module):
-    def __init__(self):
+    def __init__(self, hps):
         super().__init__()
-        self.conv = nn.Sequential(nn.Conv2d(3, 16, kernel_size=3, stride=2, padding=1),
+        self.conv = nn.Sequential(nn.Conv2d(hps.channels, 64, kernel_size=3, stride=1, padding=1),
                                   nn.ELU(inplace=True),
-                                  nn.Conv2d(16, 32, kernel_size=3, stride=2, padding=1),
+                                  nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1),
+                                  nn.ELU(inplace=True),
+                                  nn.Conv2d(128, 128, kernel_size=3, stride=2, padding=1),
+                                  nn.ELU(inplace=True),
+                                  nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=1),
                                   nn.ELU(inplace=True))
 
-        self.linear = nn.Sequential(nn.Linear(2048, 512),
-                                    nn.ELU(inplace=True),
-                                    nn.Linear(512, 1),
+        self.linear = nn.Sequential(nn.Linear(4096, 1),
                                     nn.Sigmoid())
 
     def forward(self, x):
