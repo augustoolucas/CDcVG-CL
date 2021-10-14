3453e6efa4b3519faec11e25b6ef32c64b81329c
lucas.a.alcantara <lucas.a.alcantara@accenture.com>
2021-10-14 16:52:28 -0300

    get_dataloader now handles multiple tasks

--- pseudo-replay-cl/data_utils.py
+++ pseudo-replay-cl/data_utils.py
@@ -5,6 +5,12 @@ import torch
 import copy
 import random
 
+def onehot_encoder(labels, n_classes):
+    labels_onehot = torch.zeros(labels.shape[0], n_classes)
+    labels_onehot.scatter_(1, labels.view(-1, 1), 1)
+
+    return labels_onehot
+
 def load_data(dataset, val=False):
     if dataset == 'MNIST':
         train_set = datasets.MNIST(root='./Datasets',
@@ -47,7 +53,7 @@ def load_tasks(dataset, val=False):
 
 def create_tasks(dataset):
     n_classes = len(set(dataset.targets.tolist()))
-    task_labels = get_tasks_labels(n_classes)
+    task_labels = [[x, x+1] for x in range(0, n_classes, 2)]
     datasets = []
 
     for labels in task_labels:
@@ -59,20 +65,33 @@ def create_tasks(dataset):
      
     return datasets
 
-def get_tasks_labels(n_classes):
-    return [[x, x+1] for x in range(0, n_classes, 2)]
 
-def get_task_data_shape(data):
-    return data[0][0][0].shape
+def get_tasks_labels(tasks):
+    tasks_labels = [list(set(task.targets.tolist())) for task in tasks]
+    return tasks_labels
 
-def get_task_n_classes(data):
-    n_classes = 0
-    for task in data:
-        n_classes += len(set(task.targets.tolist()))
-        
-    return n_classes
+
+def get_task_data_shape(tasks):
+    return tasks[0][0][0].shape
+
+def get_tasks_classes(tasks):
+    classes = [class_ for task in tasks for class_ in set(task.targets.tolist())]
+    return classes
+
 
 def get_dataloader(dataset, batch_size):
+    if type(dataset) == list:
+        data = torch.tensor([], dtype=dataset[0].data.dtype)
+        targets = torch.tensor([])
+
+        for ds in dataset:
+            data = torch.cat([data, ds.data])
+            targets = torch.cat([targets, ds.targets])
+
+        dataset = copy.deepcopy(dataset[0])
+        dataset.data = data
+        dataset.targets = targets
+
     loader = DataLoader(dataset,
                         batch_size,
                         num_workers=0,
@@ -91,6 +110,12 @@ def update_train_set(dataset, new_images, new_labels):
             # TODO: Raise an exception instead
             print('Unknown error')
             exit()
+
+    if dataset.data.dtype != new_images.dtype:
+        if dataset.data.dtype == torch.uint8 and new_images.dtype == torch.float32:
+            new_images = (new_images * 255).type(dataset.data.dtype)
+        else:
+            print('Warning: Mismatched data types.')
 
     dataset.data = torch.cat([dataset.data, new_images], dim=0)
     dataset.targets = torch.cat([dataset.targets, new_labels], dim=0)
