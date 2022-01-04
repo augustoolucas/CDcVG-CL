52dc006f6c0e359a9063c5052e034ac521f368e5
lucas.a.alcantara <lucas.a.alcantara@accenture.com>
2022-01-04 16:37:15 -0300

    minor refactoring on data balancing

--- pseudo-replay-cl/utils/data.py
+++ pseudo-replay-cl/utils/data.py
@@ -84,7 +84,8 @@ def create_task(dataset):
         idxs = []
         for label in labels:
             all_idxs = np.nonzero(np.isin(dataset.targets, [label]))[0]
-            idxs.extend(all_idxs[:5000])
+            all_idxs = all_idxs[:5000]
+            idxs.extend(all_idxs)
         task_set = copy.deepcopy(dataset)
         if type(dataset.targets) is list:
             task_set.targets = np.array(dataset.targets)[idxs].tolist()
